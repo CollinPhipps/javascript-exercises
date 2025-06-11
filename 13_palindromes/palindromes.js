8eac54a5310b92a49a5542aaa16575eb328ef18f
Collin Phipps <phippscollin@gmail.com>
2025-06-11 14:48:05 -0600

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.split("").filter((char) => /^[a-zA-Z]$/.test(char) || /^[0-9]$/.test(char)).map((char) => char.toLowerCase());
 
+    l = 0;
+    r = word.length - 1;
+    while (l <= r) {
+        if (word[l] !== word[r])
+            return false;
+
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
