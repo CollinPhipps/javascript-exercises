cbb8b1f18c3d2fe01040771d9861a0939f9046c0
Collin Phipps <phippscollin@gmail.com>
2025-06-11 15:16:46 -0600

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYearOfDeath = new Date().getFullYear();
+    let maxYearOfDeath = new Date().getFullYear();
+    return people.reduce((max, person) =>  {
+        if (("yearOfDeath" in person)) 
+            currentYearOfDeath = person.yearOfDeath;
+        if (("yearOfDeath" in max))
+            maxYearOfDeath = max.yearOfDeath;
 
+        const maxAge = maxYearOfDeath - max.yearOfBirth;
+        const currentAge = currentYearOfDeath - person.yearOfBirth;
+        
+        return currentAge > maxAge ? person : max;
+    });
 };
 
 // Do not edit below this line
